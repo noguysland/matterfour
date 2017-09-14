4dd0ddc8acfe0df7e42b8d233fcbe712d4a90419
seLain <selain@nature.ee.ncku.edu.tw>
2017-09-15 00:16:42 +0800

    typo fix

--- build/lib/matterfour/mattermost.py
+++ build/lib/matterfour/mattermost.py
@@ -4,4 +4,4 @@ class MattermostAdapter(object):
 	def __init__(self, mm_url, bot_acc, bot_passwd, bot_teams=[], sl_verify=False):
 		self.teams = bot_teams
 		self.account = bot_acc
-		self.password = bot_password
+		self.password = bot_passwd
